db76912b83a8b7b0eab2d872cfe869ee366e6a1b
darkknight77 <shaiklatheefmrf98@gmail.com>
2018-11-10 09:29:45 +0530

    workspace files Sat 11/10/2018  9:29:34.98

--- Practice-11-Nov2018/src/com/data/fibonnaciRecursion.java
+++ Practice-11-Nov2018/src/com/data/fibonnaciRecursion.java
@@ -2,9 +2,27 @@ package com.data;
 
 public class fibonnaciRecursion {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	static int a=0,b=1,c=1;	
 
+	private static void fibonnaci(int count) {
+	// TODO Auto-generated method stub
+      if(count>0){
+		c=a+b;
+		System.out.print(" "+c);
+		a=b;
+		b=c;
+		fibonnaci(count-1);
+	
+   }
+}
+	
+	
+	public static void main(String[] args) {
+		// 0, 1, 1, 2, 3, 5, 8, 13, 21
+		
+		System.out.print(a+" "+b);
+		fibonnaci(8);
+		
 	}
 
 }
