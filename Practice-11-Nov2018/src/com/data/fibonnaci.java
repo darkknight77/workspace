4ebba6334b7eaadee5791a933717df2ea3058a35
darkknight77 <shaiklatheefmrf98@gmail.com>
2018-11-10 09:15:08 +0530

    workspace files Sat 11/10/2018  9:15:01.43

--- Practice-11-Nov2018/src/com/data/fibonnaci.java
+++ Practice-11-Nov2018/src/com/data/fibonnaci.java
@@ -5,13 +5,14 @@ public class fibonnaci {
 	public static void main(String[] args) {
 		//0, 1, 1, 2, 3, 5, 8, 13, 21
 	
-	System.out.println("0");
+	System.out.print("0 1 ");
 	int a,b,c=1;
-	while(c<25){
 	 a=0;
 	 b=1;
+	while(c<25){
+	
 	 c=a+b;
-	System.out.print(c);
+	System.out.print(c+" ");
 	a=b;
 	b=c;
 	}
