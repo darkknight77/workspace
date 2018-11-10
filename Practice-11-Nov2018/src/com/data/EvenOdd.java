ca1a33cc6849efed19de8cf42b91f77cdb623d1f
darkknight77 <shaiklatheefmrf98@gmail.com>
2018-11-10 08:59:45 +0530

    workspace files Sat 11/10/2018  8:59:35.67

---
+++ Practice-11-Nov2018/src/com/data/EvenOdd.java
@@ -0,0 +1,30 @@
+package com.data;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		
+		for (int i = 1; i < 100; i++) {
+			
+				
+				if(i%2==0)
+				{
+				  System.out.print("Even: "+i);	
+				}
+					
+		}
+		
+		for (int i = 1; i < 100; i++) {
+			
+			
+			if(i%2!=0)
+			{
+			  System.out.print("Odd: "+i);	
+			}
+			
+		
+		
+	}
+	}
+	
+}
