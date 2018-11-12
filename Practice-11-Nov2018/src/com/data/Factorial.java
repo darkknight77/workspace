31b2e16ca402967fb3342be25f21a6c235ed805b
darkknight77 <shaiklatheefmrf98@gmail.com>
2018-11-12 13:58:21 +0530

    workspace files Mon 11/12/2018 13:58:16.95

---
+++ Practice-11-Nov2018/src/com/data/Factorial.java
@@ -0,0 +1,19 @@
+package com.data;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int n=5;
+		int factorial = 0;
+		
+		while(n>0)
+		{
+			
+			factorial=n*(n-1);
+		}
+		
+	}
+
+}
