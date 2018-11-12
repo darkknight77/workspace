e8002c12c45f2cbc13fb5279c86e0f3cc0384f95
darkknight77 <shaiklatheefmrf98@gmail.com>
2018-11-12 13:08:20 +0530

    workspace files Mon 11/12/2018 13:08:16.52

---
+++ Practice-11-Nov2018/src/com/data/pattern4.java
@@ -0,0 +1,24 @@
+package com.data;
+
+public class pattern4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int n=1;
+		
+		for (int i = 0; i < 5; i++) {
+		
+			
+			for (int j = 0; j < n; j++) {
+				
+				
+				
+			}
+		}
+		
+		
+		
+	}
+
+}
