f3258c696b8c692f4b32452d57322fa16259d768
darkknight77 <shaiklatheefmrf98@gmail.com>
2018-11-12 14:13:22 +0530

    workspace files Mon 11/12/2018 14:13:17.06

--- Practice-11-Nov2018/src/com/data/Factorial.java
+++ Practice-11-Nov2018/src/com/data/Factorial.java
@@ -5,18 +5,16 @@ public class Factorial {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int n=5;
+		int n = 10;
 		int factorial = 1;
-		
-		while(n>1)
-		{
-			
-			factorial*=n*(n-1);
-			System.out.println("e: "+factorial);
-			n-=2;
+
+		while (n > 1) {
+
+			factorial *= n * (n - 1);
+			n -= 2;
 		}
 		System.out.println(factorial);
-		
+
 	}
 
 }
