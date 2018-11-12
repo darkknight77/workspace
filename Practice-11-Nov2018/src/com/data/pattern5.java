5c573af188850a3362f04b7f4f6431b936c3d806
darkknight77 <shaiklatheefmrf98@gmail.com>
2018-11-12 13:18:22 +0530

    workspace files Mon 11/12/2018 13:18:18.02

--- Practice-11-Nov2018/src/com/data/pattern5.java
+++ Practice-11-Nov2018/src/com/data/pattern5.java
@@ -5,6 +5,22 @@ public class pattern5 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int s = 3, n = 1;
+
+		for (int i = 0; i < 5; i++) {
+
+			for (int j = 0; j < s; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < n; j++) {
+				System.out.print("*");
+			}
+
+			s--;
+			n++;
+			System.out.println();
+		}
+
 	}
 
 }
