16d522d646d09eb12d48dc901933eee4dee93a4d
darkknight77 <shaiklatheefmrf98@gmail.com>
2018-11-12 20:13:27 +0530

    workspace files Mon 11/12/2018 20:13:20.35

--- Practice-11-Nov2018/src/com/data/PowerOfNumber.java
+++ Practice-11-Nov2018/src/com/data/PowerOfNumber.java
@@ -1,26 +1,21 @@
 package com.data;
 
 public class PowerOfNumber {
-	 static int output = 1;
-	public static int power(int num,int power)
-	{
-		
-	while(power>1)
-	{
-		output*= num*num;
-		power-=2;
-		
-	}	
-	  
-		
+
+	public static long power(int num, int power) {
+		long output = 1;
+		while (power > 1) {
+			output *= num * num;
+			power -= 2;
+		}
+
 		return output;
 	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 		System.out.println(power(356, 4));
-		
-		
+
 	}
 
 }
