830c846a441844ace70056de79a340b288f85787
darkknight77 <shaiklatheefmrf98@gmail.com>
2018-11-12 13:33:22 +0530

    workspace files Mon 11/12/2018 13:33:16.89

--- Practice-11-Nov2018/src/com/data/pattern6.java
+++ Practice-11-Nov2018/src/com/data/pattern6.java
@@ -5,6 +5,18 @@ public class pattern6 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int n = 1;
+
+		for (int i = 0; i < 5; i++) {
+			n = 1;
+			for (int j = 0; j < n; j++) {
+
+				System.out.print(n);
+			}
+
+			n++;
+		}
+
 	}
 
 }
