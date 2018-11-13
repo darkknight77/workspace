c09c3c1c495f828b758fa572e71f642e1873e76c
darkknight77 <shaiklatheefmrf98@gmail.com>
2018-11-13 11:09:54 +0530

    workspace files Tue 11/13/2018 11:09:53.35

--- Practice-11-Nov2018/src/com/data/DuplicateNum.java
+++ Practice-11-Nov2018/src/com/data/DuplicateNum.java
@@ -5,6 +5,15 @@ public class DuplicateNum {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int[] a = {1,2,3,4,5,6,7,8,9,5};
+		
+		int sum=0;
+		for (int i = 0; i < a.length-1; i++) {
+			
+			sum+=a[i];
+		}
+		
+		System.out.println(sum);
 	}
 
 }
