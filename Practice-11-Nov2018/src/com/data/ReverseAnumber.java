2a9ac1bda0eaeee839059f97836bc7b0e76e4d13
darkknight77 <shaiklatheefmrf98@gmail.com>
2018-11-14 14:10:33 +0530

    workspace files Wed 11/14/2018 14:10:14.47

--- Practice-11-Nov2018/src/com/data/ReverseAnumber.java
+++ Practice-11-Nov2018/src/com/data/ReverseAnumber.java
@@ -5,19 +5,20 @@ public class ReverseAnumber {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int n=123;
+		int n=1235678;
 		int r=0;
+		int x=0;
 		while(n>0)
 		{
 			r=n%10;
-			System.out.println(r);
+			//System.out.println(r);
 			n=n/10;
-			System.out.println(n);
-			r=r+n;
-			System.out.println(r);
+			//System.out.println(n);
+			x=x*10+r;
+			//System.out.println(x);
 			
 		}
-		System.out.println(r);
+		System.out.println(x);
 		
 	}
 
