98df23e39e5e0da400c4351fc9bca09a2b8af3db
darkknight77 <shaiklatheefmrf98@gmail.com>
2018-11-14 13:45:05 +0530

    workspace files Wed 11/14/2018 13:45:02.52

---
+++ Practice-11-Nov2018/src/com/data/ReverseWordsInAsentence.java
@@ -0,0 +1,27 @@
+package com.data;
+
+public class ReverseWordsInAsentence {
+
+	
+	private static void ReverseWordsInAsentence() {
+		// TODO Auto-generated method stub
+
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		
+	}
+
+}
