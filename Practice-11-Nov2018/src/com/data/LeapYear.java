e639f5dffa45ca7e5e765241b283b0171318c96c
darkknight77 <shaiklatheefmrf98@gmail.com>
2018-11-14 13:10:01 +0530

    workspace files Wed 11/14/2018 13:09:59.92

--- Practice-11-Nov2018/src/com/data/LeapYear.java
+++ Practice-11-Nov2018/src/com/data/LeapYear.java
@@ -2,4 +2,38 @@ package com.data;
 
 public class LeapYear {
 
+	
+	public static boolean isLeapYear(int year)
+	{
+		boolean flag = false;
+		if(year % 400==0)
+		{
+			flag = true;
+		}
+		if(year % 100 == 0)
+		{
+			flag = false;
+		}
+		if(year % 4 == 0)
+		{
+			flag = true;
+		}
+		
+		
+		if(flag)
+		{
+			return true;
+		}
+		
+		return false;
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+	
+		
+	}
+	
 }
